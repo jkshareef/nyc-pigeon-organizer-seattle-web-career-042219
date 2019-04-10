158f7ab518714542f3d555fea35566b5177afe82
Jamal Shareef <theepie@hotmail.com>
2019-04-10 23:30:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pigeon_list = Hash.new
+  
+  data.each do |category, info|
+    info.each do |key, value|
+      pigeon_list[value] = {}
+    end
+  end
+  binding.pry
+  
+  #data.each do |category, info|
+   # info.each do |key, value|
+    #  if key.values.include?
+      
 end
